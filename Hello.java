a716b32e81599cd7d7ef82b1421a28adc1375f5c
tej731 <chaihtuteja8985@gmail.com>
2025-08-16 00:06:25 +0530

    first commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 import java.util.*;
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
+public class Hello{
+public static void main(String[] args){
+System.out.println("Hello World");
+}
 }
